5c63d96222ac6185e8385e2336a8c27f28d6b658
itaylevy <itaylevy134@gmail.com>
2023-04-23 12:31:24 +0300

    keep working

---
+++ transactions_generator.go
@@ -0,0 +1,4 @@
+/*
+This file is responsible for generating legitimate dummy transactions for testing the blockchain. 
+The workers will use it to create dummy transactions for benchmark testing.
+*/
--- transactions_generator.py
+++
@@ -1,4 +0,0 @@
-"""
-This file is responsible for generating dummy legitimate transactions for testing the blockchain. 
-Will be used to create the dummy transactions for the benchmark testing.
-"""

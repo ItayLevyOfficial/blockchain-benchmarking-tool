6f92df32099e0dea55f3964d2552aee51f744841
itaylevy <itaylevy134@gmail.com>
2023-04-23 12:29:47 +0300

    keep improving

--- cli.go
+++ cli.go
@@ -1,5 +1,4 @@
 /*
 Responsible for handling the command line command arguments and calling the appropriate functions. 
-This is the main file that is run when the program is executed. according to the parameters passed,
-it will run the appropriate logic: coordinator or worker.
+This is the main file that runs when the program is executed. According to the parameters passed, it will run the appropriate logic: coordinator or worker.	
 */
